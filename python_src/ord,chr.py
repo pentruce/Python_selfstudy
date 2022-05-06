78d3fda1f841e2363783029f144e36e0f71af37b
pentruce <bogata281@gmail.com>
2022-05-06 17:29:05 +0900

    python_src

---
+++ python_src/ord,chr.py
@@ -0,0 +1,15 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#ord(문자) : 아스키코드를 반환
+#chr(아스키코드) : 아스키코드에 맵핑된 문자를 반환
+
+print('A')
+A
+print(ord('A'))
+65
+print(chr(65))
+A
+print(chr(66))
+B
+print(ord('z'))
+122
